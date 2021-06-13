3a41e6c43f584333e96140a4ac03672b02da4b47
ivan-jb-mak <ivanjbmak@gmail.com>
2021-06-13 01:15:34 -0400

    removing initial splash animation

--- src/portfolio.js
+++ src/portfolio.js
@@ -2,7 +2,7 @@
 
 // Website related settings
 const settings = {
-  isSplash: true, // Change this to true if you want to use the splash screen.
+  isSplash: false, // Change this to true if you want to use the splash screen.
   useCustomCursor: false, // Change this to false if you want the good'ol cursor
   googleTrackingID: "",
 };
